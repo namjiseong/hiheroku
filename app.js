a475437c23de0705478af4053975eed89305a88a
namjiseong <skawltjd2000@naver.com>
2021-07-30 16:54:33 +0900

    heroku db?

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var topic = require('./lib/topic.js')
 var express = require('express');
 var app = express();
-
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/public'));
 
+exports.app = app.use(bodyParser.urlencoded({extended: false}))
 
 app.get('/', function(request, response) { 
     topic.home(request, response);
@@ -12,8 +13,12 @@ app.get('/', function(request, response) {
 app.get('/bug', function(request, response) { 
     topic.bug(request, response);
 });
-
-
+app.get('/create/bug', function(request, response) { 
+    topic.createbug(request, response);
+});
+app.post('/createbug_process', function(request, response){
+    topic.createbug_process(request, response);
+})
 app.listen(process.env.PORT || 5500, function(){ 
     console.log(`start with port: 5500`);
   });
