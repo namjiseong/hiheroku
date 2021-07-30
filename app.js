416836fe6ede1ff9c7a979d2eb2e4e628cc3efb1
namjiseong <skawltjd2000@naver.com>
2021-07-30 15:31:30 +0900

    db추가 전 테스트

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ var express = require('express');
 var app = express();
 
 
+app.use(express.static(__dirname + '/public'));
+
 
 app.get('/', function(request, response) { 
     topic.home(request, response);
 });
+app.get('/bug', function(request, response) { 
+    topic.bug(request, response);
+});
+
 
 app.listen(process.env.PORT || 5500, function(){ 
     console.log(`start with port: 5500`);
